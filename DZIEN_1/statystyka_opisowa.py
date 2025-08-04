f2481f530de6278d5e30259521390e9a69eb7a82
albim72 <marcin.albiniak72@gmail.com>
2025-08-04 14:29:42 +0200

    Update statystyka_opisowa.py

--- DZIEN_1/statystyka_opisowa.py
+++ DZIEN_1/statystyka_opisowa.py
@@ -2,3 +2,13 @@ import random
 
 populacja = ["Anna","Bartek","Celina","Marek","Damian","Jakub","Kasia","Michał","Ola","Piotr","Ryszard",
              "Tomasz","Witold","Zdzisiek"]
+
+proba = random.sample(populacja, 5)
+print(f"proba badawcza: {proba}")
+
+print("_________________________________________________________________________________")
+
+import numpy as np
+
+punkty = [65,65,34,899,0,25,822,5,8,32,79,112,573,277]
+srednia 
