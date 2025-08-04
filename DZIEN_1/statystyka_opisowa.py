a796c7c07fbe7749c27400ee89d0fb5a45cb6fc7
albim72 <marcin.albiniak72@gmail.com>
2025-08-04 14:34:21 +0200

    Update statystyka_opisowa.py

--- DZIEN_1/statystyka_opisowa.py
+++ DZIEN_1/statystyka_opisowa.py
@@ -11,4 +11,14 @@ print("_________________________________________________________________________
 import numpy as np
 
 punkty = [65,65,34,899,0,25,822,5,8,32,79,112,573,277]
-srednia 
+srednia = np.mean(punkty)
+odchylenie = np.std(punkty)
+mediana = np.median(punkty)
+
+print(f"srednia: {srednia}")
+print(f"odchylenie: {odchylenie}")
+print(f"mediana: {mediana}")
+
+print("_________________________________________________________________________________")
+
+kolory = ["red","green","blue","yellow","orange","purple","brown","pink","gray","cyan","magenta"]
