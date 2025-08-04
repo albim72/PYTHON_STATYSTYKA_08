6dc2f114642c31582a1a86fc4b50f6d2a92b607c
albim72 <marcin.albiniak72@gmail.com>
2025-08-04 14:38:29 +0200

    Update statystyka_opisowa.py

--- DZIEN_1/statystyka_opisowa.py
+++ DZIEN_1/statystyka_opisowa.py
@@ -21,7 +21,21 @@ print(f"mediana: {mediana}")
 
 print("_________________________________________________________________________________")
 
+#skala nominalna: kolory samochodów
 kolory = ["red","green","blue","yellow","orange","purple","brown","pink","gray","cyan","magenta"]
 
 #skala kategorialna: rodzaj samochodów
 rodzaje = ["sedan","hatchback","suv","truck","convertible","van","pickup","wagon","minivan"]
+
+#skala porządkowa
+stopnie_wojskowe = ["szeregowy","kapral","porucznik","kapitan"]
+
+#skala ilorazowa
+wzrosty = [153,177,184,190,145,167,180,201,124]
+print(f"skala nominalna: {kolory}")
+print(f"skala kategorialna: {rodzaje}")
+print(f"skala porządkowa: {stopnie_wojskowe}")
+print(f"skala ilorazowa: {wzrosty}")
+
+
+
