7fe3cab8cea8c002e3f1335c268c62daf5da6b41
albim72 <marcin.albiniak72@gmail.com>
2025-08-04 13:03:22 +0200

    Update obiekt.py

--- DZIEN_1/obiekt.py
+++ DZIEN_1/obiekt.py
@@ -4,14 +4,24 @@ class Book:
         self.book_title = title
         self.author = author
         self.pages = pages
-        self.cena = 0
+        self._cena = 0
         self.newbook()
-        
+
+    def __repr__(self):
+        return f"{self.book_title} - {self.author}"
+
     def newbook(self):
         print("Nowa książka: Book")
 
     def cena(self):
-        print(f"Cena: {self.cena} zł")
-        
+        print(f"Cena: {self._cena} zł")
+
     def setcena(self,nowacena):
-        self.cena = nowacena
+        self._cena = nowacena
+
+book1 = Book("Python", "Michal", 100)
+book1.cena()
+book1.setcena(101.50)
+book1.cena()
+
+print(book1)
