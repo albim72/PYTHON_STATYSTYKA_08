e6f66362e0452dabb93b055840e30a8f6c359d57
albim72 <marcin.albiniak72@gmail.com>
2025-08-05 11:39:42 +0200

    Create kule.py

---
+++ DZIEN_2/kule.py
@@ -0,0 +1,13 @@
+#prawdopodobienstwo wylosowania białej kuli -> 5 białych i 3 czarne
+
+p1 = 5/8
+
+#prawdopodobienstwo kolejnej białekj bez zwracania
+
+p2 = 4/7
+
+#prawdopodobinstwo łączne
+p_laczne = p1*p2
+
+print(f"prawdopodobienstwo wylosowania białej kul: {p1}")
+print(f"prawdopodobienstwo wylosowania dwóch białych kul: {p_laczne}")
